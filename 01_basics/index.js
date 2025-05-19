ff637eb7339e47808b2d7bce349749076f0dca78
VanshNazwani <nazwaniv@gmail.com>
2025-05-20 05:19:46 +0530

    Add files via upload

---
+++ 01_basics/index.js
@@ -0,0 +1,9 @@
+console.log("hello vansh ");
+
+console.log(4555+65656);
+console.log("i'm start learning node js ");
+console.log("let's start");
+
+// npm i -g nodemon is used to run commands automatically 
+// npm init -y : is used to make package.json file  
+// npm i nodemon : used to import package-lock-json file 
