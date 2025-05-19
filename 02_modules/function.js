83f323c167fc5117ccf465b2579f18c8b1ca69f0
VanshNazwani <nazwaniv@gmail.com>
2025-05-20 05:21:26 +0530

    Add files via upload

---
+++ 02_modules/function.js
@@ -0,0 +1,8 @@
+
+export const sum = (a,b) => {
+    console.log(`sum of ${a} & ${b} is ${a+b}`);
+}
+
+export const sub = (a,b) => {
+    console.log(`subtraction of ${a} & ${b} is ${a-b}`);
+}
---
+++ 02_modules/index.js
@@ -0,0 +1,6 @@
+console.log("this is lecture 2");
+
+import {sum,sub} from './function.js'
+
+sum(10,50);
+sub(10,50);
